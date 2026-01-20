000bea5a0d5e51dd1f09a4f5e8a1f95b13faf5d5
krish virani <viranikrish27@gmail.com>
2026-01-20 18:15:23 +0530

    added async handler in route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,10 +3,10 @@ const route = require("express").Router();
 const { register, login, logout } = require("../controllers/auth.controller");
 const { registerValidation, loginValidation } = require("../validators/auth.validator");
 const validate = require("../middlewares/validate.middleware");
+const asyncHandler = require("../utils/asyncHandler");
 
-route.post("/register", registerValidation, validate, register);
-route.post("/login", loginValidation, validate, login);
-route.post("/logout", logout);
-
+route.post("/register", registerValidation, validate, asyncHandler(register));
+route.post("/login", loginValidation, validate, asyncHandler(login));
+route.post("/logout", asyncHandler(logout));
 
 module.exports = route;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,9 +2,8 @@ const route = require("express").Router();
 
 const { profile } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
+const asyncHandler = require("../utils/asyncHandler");
 
-
-route.get("/profile", authMiddleware, profile);
-route.get("/profile", authMiddleware, profile);
+route.get("/profile", authMiddleware, asyncHandler(profile));
 
 module.exports = route;
